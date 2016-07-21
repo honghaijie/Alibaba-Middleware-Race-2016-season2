df84201329216d28715aff215ee2143d02d1ac27
honghaijie02 <honghaijie@gmail.com>
2016-07-21 15:16:40 +0800

    change buffer size to 16

--- src/main/java/com/alibaba/middleware/race/OrderSystemImpl.java
+++ src/main/java/com/alibaba/middleware/race/OrderSystemImpl.java
@@ -592,7 +592,7 @@ public class OrderSystemImpl implements OrderSystem {
                 */
                 MappedByteBuffer rfc = mbbMap.get(fileIdMapperRev.get((int) fileId));
                 synchronized (rfc) {
-                    BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteBufferBackedInputStream(rfc, (int)rawOffset), "UTF-8"), 256);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(new ByteBufferBackedInputStream(rfc, (int)rawOffset), "UTF-8"), 16);
                     ans.add(reader.readLine());
                 }
                 //reader.close();
